95b89fe954976101a4ff839ccccf23b047077b4c
Svyatoslafff <svjatoslavkom12@gmail.com>
2025-02-14 20:32:09 +0200

    add redux-persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import css from './App.module.scss';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact, deleteContact } from '../../redux/contactsSlice';
 import { changeFilter } from '../../redux/filtersSlice';
@@ -14,10 +13,6 @@ function App() {
     const contacts = useSelector(state => state.contacts.items).filter(item =>
         item.name.toLowerCase().includes(filter.toLowerCase())
     );
-
-    useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }, [contacts]);
 
     function onAddContact(newContact) {
         dispatch(addContact(newContact));
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,42 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
-        filters: filtersReducer,
+        contacts: persistReducer(persistConfig, contactsReducer),
+        filters: persistReducer(persistConfig, filtersReducer),
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
+
+export let persistor = persistStore(store);
